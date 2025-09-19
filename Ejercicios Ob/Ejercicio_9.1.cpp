d61086ecf630fe5faeddc5f4b01d409d8b50f76b
liseth-oss <liseth.zarate-t@mail.escuelaing.edu.co>
2025-09-19 12:55:04 -0500

    Ejercicios

---
+++ Ejercicios Ob/Ejercicio_9.1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int mcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    cout << "El MCD de " << num1 << " y " << num2 << " es: " << mcd(num1, num2) << endl;
+
+    return 0;
+}
