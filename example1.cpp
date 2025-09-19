eb5c82dec218971fd96232f55d8b97b8f1c0413c
Liseth-oss <liseth.zarate-t@mail.escuelaing.edu.co>
2025-09-19 13:40:15 -0500

    Delete example1.cpp

--- example1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hola, este es un mensaje modificado por el programa." << std::endl;
-    std::cout << "¡Bienvenido a C++!" << std::endl;
-    return 0;
-}
